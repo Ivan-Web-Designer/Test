745f7e1a249bc8c77b86518d3f33e5e519120aa5
Ivan <66520556+Ivan-Web-Designer@users.noreply.github.com>
2021-09-02 14:57:10 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:true,
+		dots:true,
+		adaptiveHeight:true,
+		autoplay:true,
+		autoplayspeed:3000,
+		pauseOnFocus:true,
+		pauseOnHover:false,
+		pauseOnDotHover:true,
+		waitForAnimate:false,
+		centerMode: true,
+		variableWidth:true,
+	});
+});
